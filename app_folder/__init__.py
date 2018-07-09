f6ab16e38a50107288afe8e1f90952dcece1126a
Melody <meldye@indiana.edu>
2018-07-08 23:07:00 -0500

    In which our hero is outcopped

---
+++ app_folder/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+
+import sys
+sys.path.insert(0, '/Documents/Insight/flask_app/app_folder')
+
+from keysecret import keys
+
+app = Flask(__name__, instance_path=keys['path'])
+app.config['SECRET_KEY'] = keys['secret']
